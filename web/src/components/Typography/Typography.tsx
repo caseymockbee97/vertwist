5ba699b1ec8598dfd506781ef92efbe1f9d74023
Casey Mockbee <caseymockbee97@gmail.com>
2023-09-14 07:33:47 -0400

    Added Typography Component

---
+++ web/src/components/Typography/Typography.tsx
@@ -0,0 +1,14 @@
+interface Props {
+
+}
+
+const Typography = ({}: Props) => {
+  return (
+    <div>
+      <h2>{'Typography'}</h2>
+      <p>{'Find me in ./web/src/components/Typography/Typography.tsx'}</p>
+    </div>
+  )
+}
+
+export default Typography
