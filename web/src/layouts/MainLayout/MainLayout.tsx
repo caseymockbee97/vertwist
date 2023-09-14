d051c7cbf5303216ff4e5a151afa33a223e138d0
Casey Mockbee <caseymockbee97@gmail.com>
2023-09-14 07:49:53 -0400

    Updated storybook config to work with styled-components.

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,18 @@
+import { TANGAROA_500 } from "src/design/Colors"
+import styled from "styled-components"
+
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <>{children}</>
+  return <Container>{children}</Container>
 }
 
-
+const Container = styled.div`
+  background-color: ${TANGAROA_500};
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
+`
 export default MainLayout
