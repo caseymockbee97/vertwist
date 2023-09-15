ac48f9ee502e29556a10dbe4882046ae7bc4ad4d
Casey Mockbee <caseymockbee97@gmail.com>
2023-09-15 08:27:50 -0400

    Updated Typography

--- web/src/components/Typography/Typography.tsx
+++ web/src/components/Typography/Typography.tsx
@@ -1,9 +1,7 @@
 import { ORANGE_500 } from 'src/design/Colors'
 import styled from 'styled-components'
 
-
-
-type TypographyVariant = 'h1' | 'h2' | 'p'
+type TypographyVariant = 'h1' | 'h2' | 'h3' | 'p'
 
 interface TypographyProps {
   children?: React.ReactNode
@@ -16,6 +14,8 @@ const Typography = ({ children, variant }: TypographyProps) => {
       return <StyledH1>{children}</StyledH1>
     case 'h2':
       return <StyledH2>{children}</StyledH2>
+    case 'h3':
+      return <StyledH3>{children}</StyledH3>
     case 'p':
       return <StyledP>{children}</StyledP>
   }
@@ -24,13 +24,22 @@ const Typography = ({ children, variant }: TypographyProps) => {
 const StyledH1 = styled.h1<{ color?: string }>`
   color: ${({ color }) => (color ? color : ORANGE_500)};
   font-family: Lato;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 32px; /* 100% */
+  letter-spacing: -0.96px;
+`
+const StyledH2 = styled.h2<{ color?: string }>`
+  color: ${({ color }) => (color ? color : ORANGE_500)};
+  font-family: Lato;
   font-size: 24px;
   font-style: normal;
   font-weight: 700;
   line-height: 24px; /* 100% */
   letter-spacing: -0.72px;
 `
-const StyledH2 = styled.h2<{ color?: string }>`
+const StyledH3 = styled.h3<{ color?: string }>`
   color: ${({ color }) => (color ? color : ORANGE_500)};
   font-family: Lato;
   font-size: 16px;
