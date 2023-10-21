489e25877db14074ad92dfc6bc448e7977ffd1a7
Casey Mockbee <caseymockbee97@gmail.com>
2023-10-20 21:43:46 -0400

    Cleaned it up a bit.

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,34 +1,20 @@
-import { navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import ColorTiles from 'src/components/ColorTiles'
-import DefaultInput from 'src/components/DefaultInput/DefaultInput'
-import LargeButton from 'src/components/LargeButton/LargeButton'
-import SmallButton from 'src/components/SmallButton/SmallButton'
 import Title from 'src/components/Title/Title'
-import {
-  HOLD_BLACK,
-  HOLD_BLUE,
-  HOLD_GREEN,
-  HOLD_ORANGE,
-  HOLD_PINK,
-  HOLD_PURPLE,
-  HOLD_RED,
-  HOLD_WHITE,
-  HOLD_WILDCARD,
-  HOLD_YELLOW,
-  WHITE,
-} from 'src/design/Colors'
-import { StyledH1, StyledH2, StyledH3, StyledP } from 'src/design/Typography'
+import { WHITE } from 'src/design/Colors'
+import { StyledH2, StyledH3 } from 'src/design/Typography'
 import styled from 'styled-components'
 
 const HomePage = () => {
   return (
     <>
-      <MetaTags title="Home" description="Vertical Scramble. The card game for climbers." />
+      <MetaTags
+        title="Home"
+        description="Vertical Scramble. The card game for climbers."
+      />
 
       <Container $gap="36px">
         <Title />
-        <Container $gap="12px">
+        <Container $gap="18px">
           <StyledH2>The card game for climbers.</StyledH2>
           <StyledH3 color={WHITE}>Coming soon...</StyledH3>
         </Container>
