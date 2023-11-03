341fde8f23058f4c678af874f37d2211b3b53d71
Casey Mockbee <caseymockbee97@gmail.com>
2023-11-03 19:47:07 -0400

    Added an Email Input.

--- web/src/components/DefaultInput/DefaultInput.tsx
+++ web/src/components/DefaultInput/DefaultInput.tsx
@@ -1,8 +1,44 @@
-import { TextField } from '@redwoodjs/forms'
+import { EmailField, TextField } from '@redwoodjs/forms'
 import { TANGAROA_100, TANGAROA_500 } from 'src/design/Colors'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+// TODO: Look up how to use this when you have wifi.
+const inputStyles = css<{ width?: string }>`
+  padding: 6px 12px;
+  border-radius: 5px;
+  width: ${({ width }) => (width ? width : 'auto')};
+
+  /* FONT H3 */
+  font-family: Lato;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px; /* 100% */
+  letter-spacing: -0.48px;
+  color: ${TANGAROA_500};
+  &::placeholder {
+    color: ${TANGAROA_100};
+  }
+`
 
 const DefaultInput = styled(TextField)<{ width?: string }>`
+  padding: 6px 12px;
+  border-radius: 5px;
+  width: ${({ width }) => (width ? width : 'auto')};
+
+  /* FONT H3 */
+  font-family: Lato;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px; /* 100% */
+  letter-spacing: -0.48px;
+  color: ${TANGAROA_500};
+  &::placeholder {
+    color: ${TANGAROA_100};
+  }
+`
+export const EmailInput = styled(EmailField)<{ width?: string }>`
   padding: 6px 12px;
   border-radius: 5px;
   width: ${({ width }) => (width ? width : 'auto')};
