52bb5aee172712f16f0cf3e75e3ab54ec6927b79
Casey Mockbee <caseymockbee97@gmail.com>
2023-12-27 20:46:18 -0500

    Added active state to the navbar.

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,21 +1,46 @@
 import styled from '@emotion/styled'
 
-import { Link, routes } from '@redwoodjs/router'
+import { Link, routes, useMatch } from '@redwoodjs/router'
 
-import { TANGAROA_500, ORANGE_300, TANGAROA_400 } from 'src/design/Colors'
+import {
+  TANGAROA_500,
+  ORANGE_300,
+  TANGAROA_400,
+  ORANGE_500,
+  WHITE,
+} from 'src/design/Colors'
 import { StyledH2 } from 'src/design/Typography'
 
 const NavBar = () => {
+  const isHomeActive = useMatch(routes.home())
+  const isGameplayActive = useMatch(routes.gameplay())
+  const isFaqActive = useMatch(routes.faq())
+
   return (
     <Container>
       <UnstyledLink to={routes.home()}>
-        <StyledH2 hoverColor={ORANGE_300}>Home</StyledH2>
+        <StyledH2
+          color={isHomeActive.match ? WHITE : ORANGE_500}
+          hoverColor={ORANGE_300}
+        >
+          Home
+        </StyledH2>
       </UnstyledLink>
       <UnstyledLink to={routes.gameplay()}>
-        <StyledH2 hoverColor={ORANGE_300}>Gameplay</StyledH2>
+        <StyledH2
+          color={isGameplayActive.match ? WHITE : ORANGE_500}
+          hoverColor={ORANGE_300}
+        >
+          Gameplay
+        </StyledH2>
       </UnstyledLink>
       <UnstyledLink to={routes.faq()}>
-        <StyledH2 hoverColor={ORANGE_300}>FAQ</StyledH2>
+        <StyledH2
+          color={isFaqActive.match ? WHITE : ORANGE_500}
+          hoverColor={ORANGE_300}
+        >
+          FAQ
+        </StyledH2>
       </UnstyledLink>
     </Container>
   )
